2a4e561a698527df4ec39815487de839c5d68e82
Carlos <carlosjavierlopezlopez@gmail.com>
2015-06-12 23:55:51 +0200

    [flameScript]Ejemplo de directivas de preprocesamiento.
    * cambiadas las propiedades del proyecto

--- BrainsEden/Assets/Scripts/flameScript.cs
+++ BrainsEden/Assets/Scripts/flameScript.cs
@@ -3,21 +3,46 @@ using System.Collections;
 
 public class flameScript : MonoBehaviour {
 
+	private ParticleSystem sistPart;
+
 	// Use this for initialization
-	void Start () 
+	void Awake ()
 	{
+		sistPart = this.GetComponent<ParticleSystem> ();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKeyUp (KeyCode.UpArrow)) {
-			Debug.Log ("Arriba");
-			this.GetComponent<ParticleSystem>().enableEmission= true;
+
+//si se ejecuta en el editor (solamente)
+#if UNITY_EDITOR	
+		if (Input.GetKeyDown(KeyCode.Space)){
+			switchFuego(0);
 		} 
-		else if (Input.GetKeyUp (KeyCode.DownArrow)) {
-			Debug.Log ("Abajo");
-			this.GetComponent<ParticleSystem>().enableEmission= false;
+		if(Input.GetKeyUp(KeyCode.Space)){
+			switchFuego(0);
 		}
+#endif
+
+//si se ejecuta en una build de android o en el editor (tambien entra en el editor, segun unity es para hacer pruebas)
+#if UNITY_ANDROID
+		//esto es si la plataforma de ejecucion es Android (solamente), OJO!!!-> NO ES LO MISMO QUE LO ANTERIOR
+		if(Application.platform == RuntimePlatform.Android){
+			if (Input.touchCount > 0){
+				if(Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(0).phase == TouchPhase.Ended)
+					switchFuego(1);
+			}
+		}
+#endif
+	}
+
+	void switchFuego(int flag){
+		if (sistPart.enableEmission) {
+			sistPart.enableEmission = false;
+		} else {
+			sistPart.enableEmission = true;
+		}
+		Debug.Log ("ESTADO: " + sistPart.enableEmission + " DISPOSITIVO: " + flag);
 	}
 }
