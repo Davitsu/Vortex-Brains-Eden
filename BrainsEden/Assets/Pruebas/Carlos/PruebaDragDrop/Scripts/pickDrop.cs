3a43dd8f1012012fde93b34b575493e7ee2cc9f5
Carlos <carlosjavierlopezlopez@gmail.com>
2015-06-20 19:48:31 +0200

    no message

--- BrainsEden/Assets/Pruebas/Carlos/PruebaDragDrop/Scripts/pickDrop.cs
+++ BrainsEden/Assets/Pruebas/Carlos/PruebaDragDrop/Scripts/pickDrop.cs
@@ -6,10 +6,15 @@ using UnityEngine.EventSystems;		//IsPointerOverGameObject-> comprueba si un inp
 
 public class pickDrop : MonoBehaviour {
 
-	public Toggle botonCubo;
-	public Toggle botonEsfera;
+	public Button botonCubo;
+	public Button botonEsfera;
+	public GameObject cruz;
 	
-	private bool botonApretado;
+	//DEVELOPEMENT_BUILD
+	public Text inputPos;
+	public Text	textoSelec;
+	
+	private int objSeleccionado;
 	
 	void Awake(){
 		Input.multiTouchEnabled = false;
@@ -17,29 +22,82 @@ public class pickDrop : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		objSeleccionado= -1;
+		#if DEVELOPMENT_BUILD
+		inputPos.gameObject.SetActive(true);
+		textoSelec.gameObject.SetActive(true);
+		#endif
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		#if UNITY_ANDROID
+	void Update () 
+	{
+
+	}
+	
+	void LateUpdate()
+	{
 		if(Input.touchCount > 0)
 		{
-			if(Input.touches[0].phase == TouchPhase.Began && !EventSystem.current.IsPointerOverGameObject(Input.touches[0].fingerId))
+			if(Input.touches[0].position.x > 97 && objSeleccionado!= -1)	//posicion en coordenadas de world
 			{
-				if(botonCubo.isOn)
+				if(Input.touches[0].phase == TouchPhase.Began )	
 				{
-					crearObjecto(0, Vector3.zero);
+					
 				}
-				else if(botonEsfera.isOn)
+				else if(Input.touches[0].phase == TouchPhase.Moved || Input.touches[0].phase == TouchPhase.Stationary)
 				{
-					crearObjecto(1, Vector3.zero);
-				}		
+					cruz.gameObject.transform.position= Input.touches[0].position;
+					cruz.SetActive(true);
+				}
+				else if(Input.touches[0].phase == TouchPhase.Ended)
+				{
+					crearObjecto(objSeleccionado, Input.touches[0].position);
+					cruz.SetActive(false);
+				}
+				#if DEVELOPMENT_BUILD
+				inputPos.text= Input.touches[0].position.x.ToString() + " , " + Input.touches[0].position.y.ToString();
+				#endif
 			}
+			else
+			{
+				cruz.SetActive(false);
+				#if DEVELOPMENT_BUILD
+				inputPos.text= "N/A";
+				#endif
+			}
+		}
+		else
+		{
+			cruz.SetActive(false);
+			#if DEVELOPMENT_BUILD
+			inputPos.text= "N/A";
+			#endif
+		}
+		#if DEVELOPMENT_BUILD
+		if(objSeleccionado == -1)
+		{
+			textoSelec.text="Seleccion: N/A";
+		}
+		else
+		{
+			textoSelec.text="Seleccion: " + objSeleccionado;
 		}
 		#endif
 	}
-
+	
+	public void BotonApretado(int objNum)
+	{
+		if(objSeleccionado == objNum)
+		{
+			objSeleccionado= -1	;	
+		}
+		else
+		{
+			objSeleccionado= objNum;
+		}
+	}
+	
 	void crearObjecto(int type, Vector3 pos)
 	{	
 		if (type == 0) 
@@ -50,5 +108,6 @@ public class pickDrop : MonoBehaviour {
 		{
 			Debug.Log ("COLOCAR ESFERA");
 		}
+		objSeleccionado= -1;
 	}
 }
