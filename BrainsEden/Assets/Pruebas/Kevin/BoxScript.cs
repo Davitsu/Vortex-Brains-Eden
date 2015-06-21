af181b581528f87a52851da4ec84db133300b3bd
Kevin <kevinfb92@gmail.com>
2015-06-21 12:49:19 +0200

    Grid automatico

---
+++ BrainsEden/Assets/Pruebas/Kevin/BoxScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ScaleBox(float scaleX, float scaleY) {
+		this.transform.localScale = new Vector3 (scaleX, scaleY, 1);
+	}
+}
